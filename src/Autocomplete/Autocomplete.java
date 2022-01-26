f737f1c357ae4fdee9be696d3207825c10ae0a58
Tordraki <tordraki@gmail.com>
2022-01-26 12:44:44 -0800

    Initial commit

---
+++ src/Autocomplete/Autocomplete.java
@@ -0,0 +1,15 @@
+package Autocomplete;
+
+import java.util.*;
+
+public class Autocomplete {
+    public Autocomplete(){
+
+    }
+    public void addAll(Collection<? extends CharSequence> terms){
+
+    }
+    public List<CharSequence> allMatches(CharSequence prefix){
+        return null;
+    }
+}
